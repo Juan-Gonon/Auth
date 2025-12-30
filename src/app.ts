2281620d792cd4990d92d58900ac55fd53a4f075
Juan Gonon <juanfegonon27@gmail.com>
2025-12-30 14:31:29 -0600

    routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { envs } from './config'
+import { AppRouter } from './presentation/route'
 import { Server } from './presentation/server'
 ;(() => {
   main()
 })()
 
 async function main() {
-  const server = new Server({ port: envs.PORT })
+  const server = new Server({ port: envs.PORT, routes: AppRouter.routes })
 
   server.start()
 }
